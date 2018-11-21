39e7624835fd60415f1ce61630033742c01d6382
Lukáš Loukota <lukasloukota94@gmail.com>
2018-11-21 22:13:04 +0100

    Complete homework

--- week4/Objects/Automation_practice/Automation_practice.py
+++ week4/Objects/Automation_practice/Automation_practice.py
@@ -1,9 +1,32 @@
-link_women = 'href="http://automationpractice.com/index.php?id_category=3&controller=category"'
+# link_women = 'href="http://automationpractice.com/index.php?id_category=3&controller=category"'
 
-# Women category page
-span_cat_name = "class:cat-name"
-cat_name = "Women "
-product_dropdown_filter = "id:selectProductSort"
-product_list = "class:product_list"
-btn_sort_by = "id:selectProductSort"
-left_column = "id:left_column"
+# # Women category page
+# span_cat_name = "class:cat-name"
+# cat_name = "Women "
+# product_dropdown_filter = "id:selectProductSort"
+# product_list = "class:product_list"
+# btn_sort_by = "id:selectProductSort"
+# left_column = "id:left_column"
+# #Go to category women
+# link_women_category = 'class=sf-with-ul'
+
+eshop_id = "id=menu-item-40"
+button_filter = '//*[@id="woocommerce_price_filter-2"]/form/div/div[2]/button'
+category_enum = {
+    1: ["android", 1],
+    2: ["html", 3],
+    3: ["javascript", 3],
+    4: ["selenium", 1]
+}
+product_categories = '//*[@id="woocommerce_product_categories-2"]/ul'
+ul_products = '//*[@id="content"]/ul'
+
+sorting = {
+    "Sort by popularity": "Selenium Ruby",
+    "Sort by average rating": "Functional Programming in JS",
+    "Sort by newness": "HTML5 WebApp Develpment",
+    "Sort by price: low to high": "JS Data Structures and Algorithm",
+    "Sort by price: high to low": "Selenium Ruby",
+}
+
+dropdown_sort = "name:orderby"
--- week4/Tests/HW4.robot
+++ week4/Tests/HW4.robot
@@ -3,20 +3,19 @@ Library    SeleniumLibrary
 Library    Collections
 Resource    ../Keywords/eshop_keywords.txt
 Variables    ../Objects/Automation_practice/Automation_practice.py
-Test Setup    eshop_keywords.Open Browser On Women Category
-Test Teardown    eshop_keywords.Close Browser and Capture Screenshot
+Test Setup    Open Browser On Shop Category
+Test Teardown    Close Browser and Capture Screenshot
 
 
 *** Test Cases ***
-Women Category Landing Page
-    SeleniumLibrary.Page Should Contain Element    ${span_cat_name}
-    SeleniumLibrary.Page Should Contain    ${cat_name}
+Shop Landing Page
+    [Setup]    eshop_keywords.Open Browser On Base Url   
+    eshop_keywords.Go To Shop    ${eshop_id}    
 
-Go To Category
-    eshop_keywords.Go To Women Category
+Test Product Counts
+    :FOR    ${category}    IN    @{category_enum.keys()}
+    \    eshop_keywords.Select Category   ${category}
 
-Filter By Lowest Price First
-    SeleniumLibrary.Select From List By Index    ${product_dropdown_filter}    1
-    SeleniumLibrary.Wait Until Page Contains Element    ${product_list}    
-    
-    
+Sort Product by Criterion
+    :FOR    ${criterion}    IN    @{sorting}
+    \    eshop_keywords.Select Sort Criterion By Label    ${criterion}    
